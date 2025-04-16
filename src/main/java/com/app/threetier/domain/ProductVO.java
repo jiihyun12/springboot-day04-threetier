62567986aff00aa45510b57dfbba1552b496125b
jiihyun12 <j22h.h0h@gmail.com>
2025-04-16 21:00:00 +0900

    threetier 복습

--- src/main/java/com/app/threetier/domain/ProductVO.java
+++ src/main/java/com/app/threetier/domain/ProductVO.java
@@ -9,8 +9,8 @@ public class ProductVO {
     private Long id;
     private String productName;
 
-//    검증할 떄 null로 비교하기 위해 int를 Integer로 쓴다.
 //    클래스 타입의 기본 값 null
+//    검증할 떄 null로 비교하기 위해 int를 Integer로 쓴다.
     private Integer productPrice;
     private Integer productStock;
     private String productBrand;
